f571953cffc11f55fef8559793f79fdbaa860de4
phphe <phphe@outlook.com>
2024-08-22 19:20:31 +0800

    fix(types): update vite and vite-plugin-dts to fix types issue
    affects: @he-tree/vue

--- packages/he-tree-vue/types/src/App.vue.d.ts
+++
@@ -1,9 +0,0 @@
-import type { DefineComponent, ComponentOptionsMixin, EmitsOptions, VNodeProps, AllowedComponentProps, ComponentCustomProps, ExtractPropTypes } from 'vue';
-declare const _default: DefineComponent<{}, {}, {
-    items: {
-        text: string;
-        lineHeight: string;
-        width: string;
-    }[];
-}, {}, {}, ComponentOptionsMixin, ComponentOptionsMixin, EmitsOptions, string, VNodeProps & AllowedComponentProps & ComponentCustomProps, Readonly<ExtractPropTypes<{}>>, {}>;
-export default _default;
